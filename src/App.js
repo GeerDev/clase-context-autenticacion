1618bf40cc3d72bf2fcd8e2b855a5b9a2500d52d
German <gerfersie@gmail.com>
2022-06-20 12:30:49 +0200

    context authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "antd/dist/antd.css";
+
+import Login from './components/Login/Login';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Register from './components/Register/Register';
+
+import { UserProvider } from './context/UserContext/UserState';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProvider>
+        <Router>
+        <Header />
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
+      </UserProvider>
     </div>
+
   );
 }
 
